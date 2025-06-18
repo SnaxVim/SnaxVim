c689dea7ce8dc5af33ec7ceea09a4bca949194da
glmlm <91877885+glmlm@users.noreply.github.com>
2025-06-18 22:52:54 +0900

    update: Defer some settings using `vim.schedule`

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
-require("options")
+require("options_pre")
 require("configs.lazy")
 
 vim.schedule(function()
+  require("options")
   require("keymaps")
 end)
--- lua/options.lua
+++ lua/options.lua
@@ -1,32 +1,15 @@
-local g = vim.g
 local o = vim.o
 local opt = vim.opt
 
--- global variables
-g.mapleader = " "
-
--- behaves like :set
 o.confirm = true
-o.cursorline = true
 o.expandtab = true
 o.shiftwidth = 0
 o.tabstop = 2
-opt.fillchars = { eob = " ", foldclose = "", foldopen = "", foldsep = " " }
-o.foldcolumn = "1"
 o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-o.foldlevel = 99
-o.foldmethod = "expr"
-o.foldtext = ""
-o.signcolumn = "yes"
-o.statuscolumn = "%s%l%#FoldColumn#%{v:lua.require('foldcolumn')()} %*"
 o.ignorecase = true
 o.smartcase = true
-o.laststatus = 3
-o.list = true
 opt.listchars = { tab = " ‥", trail = "-", nbsp = "␣", eol = "↲", space = "·", extends = "»", precedes = "«" }
 o.mouse = "a"
-o.number = true
-o.relativenumber = true
 o.scrolloff = 5
 o.smartindent = true
 o.splitbelow = true
