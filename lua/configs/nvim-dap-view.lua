bf74b906847629d9fb9fb9ce0d94d03880c4d4b3
glmlm <91877885+glmlm@users.noreply.github.com>
2025-06-30 04:39:21 +0000

    fix: Handle changes due to update of nvim-dap-view
    See commit 1183ed9 in nvim-dap-view for details.

--- lua/configs/nvim-dap-view.lua
+++ lua/configs/nvim-dap-view.lua
@@ -1,18 +1,12 @@
 local options = {
   winbar = {
-    sections = {
-      "console",
-      "watches",
-      "scopes",
-      "exceptions",
-      "breakpoints",
-      "threads",
-      "repl",
-    },
-    default_section = "console",
     controls = {
       enabled = true,
-      position = "left",
+    },
+  },
+  windows = {
+    terminal = {
+      width = 0.35,
     },
   },
 }
