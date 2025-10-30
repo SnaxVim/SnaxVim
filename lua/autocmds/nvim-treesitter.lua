2efcce068ffb69408543b57018fe5ca5862a3e38
glmlm <91877885+glmlm@users.noreply.github.com>
2025-10-30 07:31:24 +0000

    feat(autocmd): Restrict the autocmd to specific filetypes using `pattern`

--- lua/autocmds/nvim-treesitter.lua
+++ lua/autocmds/nvim-treesitter.lua
@@ -1,14 +1,13 @@
 local api = vim.api
 api.nvim_create_autocmd("FileType", {
   group = api.nvim_create_augroup("ts-start", { clear = true }),
+  pattern = require("nvim-treesitter").get_available(),
   callback = function(args)
     local treesitter = vim.treesitter
-    local contains = vim.list_contains
-    local ts = require("nvim-treesitter")
-
     local ok_lang, lang = pcall(treesitter.language.get_lang, args.match)
     if ok_lang then
-      if contains(ts.get_available(), lang) and not contains(ts.get_installed(), lang) then
+      local ts = require("nvim-treesitter")
+      if not vim.list_contains(ts.get_installed(), lang) then
         ts.install(lang)
       end
     end
