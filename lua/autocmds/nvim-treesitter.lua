2ac56bbc7e21a11fd1a5054ed91b7ffc80a88c32
glmlm <91877885+glmlm@users.noreply.github.com>
2025-11-12 12:43:20 +0000

    fix(treesitter): Prevent crash by wrapping autocmd `callback` and `pattern` with `pcall()`

--- lua/autocmds/nvim-treesitter.lua
+++ lua/autocmds/nvim-treesitter.lua
@@ -1,14 +1,18 @@
 local api = vim.api
 api.nvim_create_autocmd("FileType", {
   group = api.nvim_create_augroup("ts-start", { clear = true }),
-  pattern = require("nvim-treesitter").get_available(),
+  pattern = (function()
+    local ok, tbl = pcall(require("nvim-treesitter").get_available)
+    return ok and tbl or "__never_match__"
+  end)(),
   callback = function(args)
     local treesitter = vim.treesitter
     local ok_lang, lang = pcall(treesitter.language.get_lang, args.match)
     if ok_lang then
       local ts = require("nvim-treesitter")
-      if not vim.list_contains(ts.get_installed(), lang) then
-        ts.install(lang)
+      local ok_installed, installed = pcall(ts.get_installed)
+      if ok_installed and not vim.tbl_contains(installed, lang) then
+        pcall(ts.install, lang)
       end
     end
 
