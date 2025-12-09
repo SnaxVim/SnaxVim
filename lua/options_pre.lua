615f2a37bf492a626d1380f59d53c830fd6e34c4
glmlm <91877885+glmlm@users.noreply.github.com>
2025-12-09 23:24:35 +0900

    refactor(options): defer some options

--- lua/options_pre.lua
+++ lua/options_pre.lua
@@ -6,9 +6,6 @@ g.mapleader = " "
 
 o.cursorline = true
 opt.fillchars = { eob = " ", foldclose = "", foldopen = "", foldsep = " " }
-o.foldlevel = 99
-o.foldmethod = "expr"
-o.foldtext = ""
 o.laststatus = 3
 o.list = true
 o.number = true
