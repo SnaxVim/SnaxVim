775f1effcc5032d88d89561ae8a4bdfbe45d0e26
glmlm <91877885+glmlm@users.noreply.github.com>
2026-03-30 02:44:45 +0000

    fix(treesitter): avoid overriding existing CC env var

--- lua/configs/nvim-treesitter.lua
+++ lua/configs/nvim-treesitter.lua
@@ -1,6 +1,7 @@
 local function config()
   if vim.uv.os_uname().sysname == "Windows_NT" and vim.fn.executable("cl") == 0 then
-    vim.env.CC = "gcc"
+    local env = vim.env
+    env.CC = env.CC or "gcc"
   end
 
   require("autocmds.nvim-treesitter")
